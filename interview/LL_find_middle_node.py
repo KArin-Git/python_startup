cf7038e8cb75a6d3dbd182bb6223d3787c016dd6
Arin <arin.apichitchon@gmail.com>
2025-03-18 09:07:59 -0400

    wording

--- interview/LL_find_middle_node.py
+++ interview/LL_find_middle_node.py
@@ -1,9 +1,13 @@
 """
 Implement the find_middle_node method for the LinkedList class.
-
 Note: this LinkedList implementation does not have a length member variable.
-
 If the linked list has an even number of nodes, return the first node of the second half of the list.
+
+Keep in mind the following requirements:
+1. The method should use a two-pointer approach, where one pointer (slow) moves one node at a time and the other pointer (fast) moves two nodes at a time.
+2. When the fast pointer reaches the end of the list or has no next node, the slow pointer should be at the middle node of the list.
+3. The method should return the middle node when the number of nodes is odd or the first node of the second half of the list if the list has an even number of nodes.
+3. The method should only traverse the linked list once.  In other words, you can only use one loop.
 """
 
 class Node:
